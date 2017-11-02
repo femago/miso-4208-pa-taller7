a60c1e287009803a6da3b909fe6bff7480eabf44
Felipe Martinez <Felipe.MartinezGonzales@wnco.com>
2017-11-01 19:07:03 -0500

    doc params

--- taller7-adb-monkey/src/main/java/co/edu/uniandes/miso4208/entry/Main.java
+++ taller7-adb-monkey/src/main/java/co/edu/uniandes/miso4208/entry/Main.java
@@ -9,7 +9,11 @@ import co.edu.uniandes.miso4208.event.EventDirectory;
 import java.util.List;
 
 public class Main {
-
+    /**
+     * -Djava.util.logging.SimpleFormatter.format="%1$tH:%1$tM:%1$tS %4$-6s %2$s:-- %5$s%6$s%n"
+     * -num 20 -evt tap,text,key,net,rotate,swipe
+     * @param args
+     */
     public static void main(String[] args) {
 
         ExecutionParameters execParameters = new ExecutionParameters(args, EventDirectory.availableEvents());
